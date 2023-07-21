814c5604d599868f51923e08b32af9033451afb2
sitogi <takano162@gmail.com>
2023-07-21 23:00:43 +0900

    Stop to use duplicate module

--- main_test.ts
+++ main_test.ts
@@ -1,4 +1,4 @@
-import { assertEquals } from "https://deno.land/std@0.195.0/testing/asserts.ts";
+import { assertEquals } from "https://deno.land/std@0.195.0/assert/mod.ts";
 import { add } from "./main.ts";
 
 Deno.test(function addTest() {
